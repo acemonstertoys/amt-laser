b83bca28e88a05aca4e8b5dba7382faa6b281d97
Peter P <pete@p373.net>
2016-02-09 01:20:51 -0800

    fixes for running in subdirectory

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,9 @@ module AmtLaser
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.relative_url_root = "/~laser/log"
+    config.action_controller.relative_url_root = "/~laser/log"
+    config.root_directory = '/~laser/log'
+    config.assets.prefix = '/~laser/log/assets/'    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,35 @@
-Rails.application.routes.draw do
-  get 'index.html', to: 'log_entries#new'
-  get 'user/index'
+require 'rails/application'
 
-  get 'user/new'
-
-  get 'user/create'
-
-  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
-  root to: "log_entries#new"
-  resources :log_entries
-  resources :users do 
-    member do 
-      post :toggle_admin
-    end
+module RouteScoper
+  # Keep the rescue so that you can revert to not having a
+  #   # subdirectory when in development and test modes
+  def self.root
+    Rails.application.config.root_directory
+  rescue NameError
+    '/'
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
 
-  # Serve websocket cable requests in-process
-  # mount ActionCable.server => '/cable'
+Rails.application.routes.draw do
+  scope RouteScoper.root do
+    get 'index.html', to: 'log_entries#new'
+    get 'user/index'
+
+    get 'user/new'
+
+    get 'user/create'
+
+    devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+    root to: "log_entries#new"
+    resources :log_entries
+    resources :users do 
+      member do 
+        post :toggle_admin
+      end
+    end
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    # Serve websocket cable requests in-process
+    # mount ActionCable.server => '/cable'
+  end
 end
